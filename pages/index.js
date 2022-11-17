ba51b9593aeedb197717915c04ee936a718e33b5
Kevin Cubas <61201131+kevinCubas@users.noreply.github.com>
2022-11-16 23:05:40 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import Layout, { siteTitle } from '../components/Layout';
+import Layout, { siteTitle } from '../components/layout';
 import Date from '../components/date';
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
